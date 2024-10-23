2f2ae977c478e126ce969d9a05b91b347fea8806
Prajyot Khandeparkar <prajyotpro@gmail.com>
2024-10-24 00:10:39 +0530

    refactored views, added test sqlite support

--- app/controller/ContentController.ts
+++ app/controller/ContentController.ts
@@ -22,14 +22,40 @@ export class ContentController extends BaseController {
     async index(@request() req: express.Request, @response() res: express.Response) {
 
         console.log("req.session.user", req.session.user)
+        const db = await openDb();
 
-        const all = await (await openDb()).all('SELECT col FROM tbl');
+        try {
+            await db.exec(`
+                CREATE TABLE test(
+                  key INTEGER PRIMARY KEY,
+                  value TEXT
+                ) STRICT
+            `);
+        } catch (error) {
+            console.log("table creation", error)
+        }
+        
+
+        // Create a prepared statement to insert data into the database.
+        const insert = await db.prepare('INSERT INTO test (key, value) VALUES (?, ?)');
+
+        // Execute the prepared statement with bound values.
+
+        try {
+            await insert.run(1, 'hello');
+            await insert.run(2, 'world');
+        } catch (error) {
+            console.log("insert error", error)
+        }
+        
+
+        const all = await db.all('SELECT * FROM test');
         console.log(all)
         // await db.client.exec('CREATE TABLE tbl (col TEXT)')
 
         res.render('content', {
             name: "Prajyot",
-            testimonials: [],
+            content: all,
             header: "",
             title: ""
         });
